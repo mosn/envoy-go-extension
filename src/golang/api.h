290df91a78f6c02091a59b91808c1d7e60076ea7
doujiang24 <doujiang24@gmail.com>
2022-09-05 19:11:25 +0800

    added __cplusplus.

--- src/golang/api.h
+++ src/golang/api.h
@@ -1,3 +1,11 @@
 #pragma once
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 void moeHttpRequestContinue(unsigned long long int filterHolder, int end);
+
+#ifdef __cplusplus
+} // extern "C"
+#endif
