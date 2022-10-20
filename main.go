28be067a38ca3c9a8f17132b6b93a2742611c690
doujiang24 <doujiang24@gmail.com>
2022-10-20 09:09:09 +0800

    rename samples.

--- main.go
+++ main.go
@@ -3,17 +3,20 @@ package main
 import (
 	"mosn.io/envoy-go-extension/pkg/http"
 	"mosn.io/envoy-go-extension/pkg/http/buffered"
-	"mosn.io/envoy-go-extension/pkg/samples/async"
-	asyncSleep "mosn.io/envoy-go-extension/pkg/samples/async-sleep"
-	bufferedSample "mosn.io/envoy-go-extension/pkg/samples/buffered"
+	"mosn.io/envoy-go-extension/samples/async"
+	asyncSleep "mosn.io/envoy-go-extension/samples/async-sleep"
+	bufferedSample "mosn.io/envoy-go-extension/samples/buffered"
 )
 
 func init() {
+	// these are useless samples
 	buffered.RegisterHttpFilterConfigFactory(bufferedSample.ConfigFactory)
 	http.RegisterHttpFilterConfigFactory(asyncSleep.ConfigFactory)
 	http.RegisterHttpFilterConfigFactory(async.ConfigFactory)
-	http.RegisterHttpFilterConfigFactory(http.PassThroughFactory)
 	buffered.RegisterHttpFilterConfigFactory(bufferedSample.ConfigFactory)
+
+	// default filter
+	http.RegisterHttpFilterConfigFactory(http.PassThroughFactory)
 }
 
 func main() {
