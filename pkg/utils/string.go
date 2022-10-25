748e6c9888a2b164a7717d330f9e333779ef8750
spacewander <spacewanderlzx@gmail.com>
2022-10-25 21:13:05 +0800

    chore: fix "possible misuse of unsafe.Pointer"
    See #7
    
    Signed-off-by: spacewander <spacewanderlzx@gmail.com>

--- pkg/utils/string.go
+++ pkg/utils/string.go
@@ -9,7 +9,7 @@ import (
 func BytesToString(ptr uint64, len uint64) string {
 	var s string
 	var sHdr = (*reflect.StringHeader)(unsafe.Pointer(&s))
-	sHdr.Data = uintptr(unsafe.Pointer(uintptr(ptr)))
+	sHdr.Data = uintptr(ptr)
 	sHdr.Len = int(len)
 	return s
 }
@@ -17,7 +17,7 @@ func BytesToString(ptr uint64, len uint64) string {
 func BytesToSlice(ptr uint64, len uint64) []byte {
 	var s []byte
 	var sHdr = (*reflect.SliceHeader)(unsafe.Pointer(&s))
-	sHdr.Data = uintptr(unsafe.Pointer(uintptr(ptr)))
+	sHdr.Data = uintptr(ptr)
 	sHdr.Len = int(len)
 	sHdr.Cap = int(len)
 	return s
