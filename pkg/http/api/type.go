27fa7e61894b88111489eec8832ddc828b48af9c
doujiang24 <doujiang24@gmail.com>
2022-11-15 15:50:11 +0800

    change: MOSN Buffer API depends on DataBufferBase API.
    introduce DataBufferBase for the common API.

--- pkg/http/api/type.go
+++ pkg/http/api/type.go
@@ -17,10 +17,6 @@
 
 package api
 
-import (
-	"mosn.io/pkg/buffer"
-)
-
 // ****************** filter status start ******************//
 type StatusType int
 
@@ -140,8 +136,83 @@ const (
 	PrependBuffer BufferAction = 2
 )
 
+type DataBufferBase interface {
+	// Write appends the contents of p to the buffer, growing the buffer as
+	// needed. The return value n is the length of p; err is always nil. If the
+	// buffer becomes too large, Write will panic with ErrTooLarge.
+	Write(p []byte) (n int, err error)
+
+	// WriteString appends the string to the buffer, growing the buffer as
+	// needed. The return value n is the length of s; err is always nil. If the
+	// buffer becomes too large, Write will panic with ErrTooLarge.
+	WriteString(s string) (n int, err error)
+
+	// WriteByte appends the byte to the buffer, growing the buffer as
+	// needed. The return value n is the length of s; err is always nil. If the
+	// buffer becomes too large, Write will panic with ErrTooLarge.
+	WriteByte(p byte) error
+
+	// WriteUint16 appends the uint16 to the buffer, growing the buffer as
+	// needed. The return value n is the length of s; err is always nil. If the
+	// buffer becomes too large, Write will panic with ErrTooLarge.
+	WriteUint16(p uint16) error
+
+	// WriteUint32 appends the uint32 to the buffer, growing the buffer as
+	// needed. The return value n is the length of s; err is always nil. If the
+	// buffer becomes too large, Write will panic with ErrTooLarge.
+	WriteUint32(p uint32) error
+
+	// WriteUint64 appends the uint64 to the buffer, growing the buffer as
+	// needed. The return value n is the length of s; err is always nil. If the
+	// buffer becomes too large, Write will panic with ErrTooLarge.
+	WriteUint64(p uint64) error
+
+	// Peek returns n bytes from buffer, without draining any buffered data.
+	// If n > readable buffer, nil will be returned.
+	// It can be used in codec to check first-n-bytes magic bytes
+	// Note: do not change content in return bytes, use write instead
+	Peek(n int) []byte
+
+	// Bytes returns all bytes from buffer, without draining any buffered data.
+	// It can be used to get fixed-length content, such as headers, body.
+	// Note: do not change content in return bytes, use write instead
+	Bytes() []byte
+
+	// Drain drains a offset length of bytes in buffer.
+	// It can be used with Bytes(), after consuming a fixed-length of data
+	Drain(offset int)
+
+	// Len returns the number of bytes of the unread portion of the buffer;
+	// b.Len() == len(b.Bytes()).
+	Len() int
+
+	// Reset resets the buffer to be empty.
+	Reset()
+
+	// String returns the contents of the buffer as a string.
+	String() string
+
+	// Append append the contents of the slice data to the buffer.
+	Append(data []byte) error
+}
+
 type BufferInstance interface {
-	buffer.IoBuffer
+	DataBufferBase
+
+	// Set overwrite the whole buffer content with byte slice.
+	Set([]byte) error
+
+	// SetString overwrite the whole buffer content with string.
+	SetString(string) error
+
+	// Prepend prepend the contents of the slice data to the buffer.
+	Prepend(data []byte) error
+
+	// Prepend prepend the contents of the string data to the buffer.
+	PrependString(s string) error
+
+	// Append append the contents of the string data to the buffer.
+	AppendString(s string) error
 }
 
 //*************** BufferInstance end **************//
